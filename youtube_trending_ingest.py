33fb1b11395d839ca3ac368b622d01bd42b7844d
aliyusifov99 <ali.yusifli0011@gmail.com>
2025-05-20 21:31:12 +0200

    chgs

--- youtube_trending_ingest.py
+++ youtube_trending_ingest.py
@@ -1,23 +1,19 @@
-import requests
-import json
 import os
+import json
 from datetime import datetime
 from dotenv import load_dotenv
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import requests
 
-
-# Load environment variables from .env
+# Load API key
 load_dotenv()
 API_KEY = os.getenv("YOUTUBE_API_KEY")
+AZURE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 
 REGION_CODE = "PL"
 MAX_RESULTS = 50
 
-# Output folder (local, to simulate Bronze)
-today = datetime.utcnow().strftime("%Y-%m-%d")
-output_dir = f"/Users/aliyusifov/Desktop/youtube_trend_analysis/datalake_demo/bronze/{today}"
-os.makedirs(output_dir, exist_ok=True)
-
-# YouTube API Endpoint
+# YouTube API call
 url = "https://www.googleapis.com/youtube/v3/videos"
 params = {
     "part": "snippet,statistics,contentDetails",
@@ -26,15 +22,22 @@ params = {
     "maxResults": MAX_RESULTS,
     "key": API_KEY
 }
-
 response = requests.get(url, params=params)
 
 if response.status_code == 200:
     data = response.json()
-    output_path = os.path.join(output_dir, "trending_videos.json")
-    with open(output_path, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
-    print(f"[✓] Trending data saved to {output_path}")
+
+    # Connect to Azure Blob
+    blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
+    container_name = "bronze"
+
+    today = datetime.utcnow().strftime("%Y-%m-%d")
+    blob_path = f"{today}/trending_videos.json"  # Azure path inside bronze
+
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_path)
+    blob_client.upload_blob(json.dumps(data, ensure_ascii=False, indent=2), overwrite=True)
+
+    print(f"[✓] Data uploaded to Azure Blob: {container_name}/{blob_path}")
 else:
     print(f"[!] Failed to fetch data: {response.status_code}")
     print(response.text)
